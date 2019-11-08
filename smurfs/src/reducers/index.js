d9afd3d087de0f0b92ab1298516dc178ee331b7e
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-11-08 11:34:14 -0600

    state and connected actions and reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,49 @@
+import {
+    FETCH_SMURFS_START,
+    FETCH_SMURFS_SUCCESS,
+    FETCH_SMURFS_FAILURE
+} from '../actions';
 
 const initialState = [{
-    date:
-}]
+    date: [{ 'name': 'Brainey', 'age': 200, 'height': '5cm', 'id': 0 }],
+    isFetching: false,
+    error: ''
+}];
+
+const smurfReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_SMURFS_START:
+            const startState = {
+                ...state,
+                isFetching: true,
+                error: ''
+            };
+            console.log('smurf startState', startState);
+            return startState;
+
+        case FETCH_SMURFS_SUCCESS:
+            const successState = {
+                ...state,
+                data: action.payload,
+                isFetching: false,
+                error: ''
+            };
+            console.log('smurf successState', successState);
+            return successState;
+
+        case FETCH_SMURFS_FAILURE:
+            const failState = {
+                ...state,
+                data: '',
+                isFetching: false,
+                error: action.payload
+            };
+            console.log('smurf failState', failState);
+            return failState;
+
+        default:
+            return state;
+    }
+}
+
+export default smurfReducer;
